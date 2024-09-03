1399e795914b65d9c34b7725398e99b8528b5baf
hardik <kantariyahardik618@ggamil.com>
2024-09-03 15:52:07 +0530

    03-09-2024

---
+++ cpp/Unary_over.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class complax{
+    private:
+    int a , b;
+    public :
+
+    void setdata(int x,int y){
+        cout<< "enter x:";
+        cin >>x; 
+        cout<< "enter y:";
+        cin >>y; 
+        a=x;
+        b=y;
+    }
+    void display(){
+        cout << "Value Of A :" << a << endl;
+        cout << "Value Of B :" << b << endl;
+    }
+    void operator-(){
+        a=-a;
+        b=-b;
+    }
+};
+int main(){
+
+    complax n;
+    n.setdata(6,-66);
+    n.display();
+    -n;
+    cout<<"after" <<endl;
+    n.display();
+
+}
