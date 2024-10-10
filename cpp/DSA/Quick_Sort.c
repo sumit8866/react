55776cfb16c9e77466fd920b76954171b850c6fc
hardik <kantariyahardik618@ggamil.com>
2024-10-10 16:46:06 +0530

    10-10-2024

--- cpp/DSA/Quick_Sort.c
+++ cpp/DSA/Quick_Sort.c
@@ -1,46 +1,67 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define n 10
-int partition(int a[],int lb,int ub){
-    int pivot=a[lb];
-    int start=lb;
-    int end=ub;
+#include <stdio.h>
+#include <stdlib.h>
+int quick(int arr[], int lb, int ub)
+{
+    int pivot = arr[lb];
+    int start = lb;
+    int end = ub;
     int temp;
 
-    while(start<end){
-        while(a[start]<=pivot){
+    while (start < end)
+    {
+        while (arr[start] <= pivot)
+        {
             start++;
-        }while(a[end]>pivot){
+        }
+        while (arr[end] > pivot)
+        {
             end--;
-        }if(start<end){
-            temp=a[start];
-            a[start]=a[end];
-            a[end]=temp;
+        }
+        if (start < end)
+        {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
         }
     }
-            temp=a[lb];
-            a[lb]=a[end];
-            a[end]=temp;
-            return end;
-    }
+    temp = arr[lb];
+    arr[lb] = arr[end];
+    arr[end] = temp;
+    return end;
+}
 
-void quicksort(int a[],int lb,int ub) {
-    if(lb<ub){
-        int loc=partition(a,lb,ub);
-        quicksort(a,lb,loc-1);
-        quicksort(a,loc+1,ub);
+void quicksort(int arr[], int lb, int ub)
+{
+    if (lb < ub)
+    {
+        int loc = quick(arr, lb, ub);
+        quicksort(arr, lb, loc - 1);
+        quicksort(arr, loc + 1, ub);
     }
 }
 
-int main(){
-    int arr[n]={50,5,30,4,80,55,1,6,7,100};
-    for(int i=0;i<10;i++){
-        printf("%d\t",arr[i]);
+int main()
+{
+    int n;
+    printf("enter array size:");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter the elements:");
+        scanf("%d", &arr[i]);
     }
-        printf("\n");
-        quicksort(arr,0,n-1);
-    for(int i=0;i<10;i++){
-        printf("%d\t",arr[i]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    quicksort(arr, 0, n - 1); // quicksort(arr, n-n, n-1);
+    printf("\n");
+    printf("after swapping\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
     }
     return 0;
 }
--- cpp/DSA/script.js
+++ cpp/DSA/script.js
@@ -1,88 +1,311 @@
-const ageCalculate = () => {
-    const today = new Date();
-    const inputDate = new Date(document.getElementById("date-input").value);
-  
-    const birthDetails = {
-      date: inputDate.getDate(),
-      month: inputDate.getMonth() + 1,
-      year: inputDate.getFullYear(),
-    };
-  
-    const currentYear = today.getFullYear();
-    const currentMonth = today.getMonth() + 1;
-    const currentDate = today.getDate();
-  
-    if (isFutureDate(birthDetails, currentYear, currentMonth, currentDate)) {
-      alert("Not Born Yet");
-      displayResult("-", "-", "-");
-      return;
+let grid = document.querySelector(".grid");
+const startButton = document.getElementById("start-button");
+const container = document.querySelector(".container");
+const coverScreen = document.querySelector(".cover-screen");
+const result = document.getElementById("result");
+const overText = document.getElementById("over-text");
+
+let matrix,
+  score,
+  isSwiped,
+  touchY,
+  initialY = 0,
+  touchX,
+  initialX = 0,
+  rows = 4,
+  columns = 4,
+  swipeDirection;
+
+let rectLeft = grid.getBoundingClientRect().left;
+let rectTop = grid.getBoundingClientRect().top;
+
+const getXY = (e) => {
+  touchX = e.touches[0].pageX - rectLeft;
+  touchY = e.touches[0].pageY - rectTop;
+};
+
+const createGrid = () => {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const boxDiv = document.createElement("div");
+      boxDiv.classList.add("box");
+      boxDiv.setAttribute("data-position", `${i}_${j}`);
+      grid.appendChild(boxDiv);
     }
-  
-    const { years, months, days } = calculateAge(
-      birthDetails,
-      currentYear,
-      currentMonth,
-      currentDate
-    );
-  
-    displayResult(days, months, years);
-  };
-  
-  const isFutureDate = (birthDetails, currentYear, currentMonth, currentDate) => {
-    return (
-      birthDetails.year > currentYear ||
-      (birthDetails.year === currentYear &&
-        (birthDetails.month > currentMonth ||
-          (birthDetails.month === currentMonth &&
-            birthDetails.date > currentDate)))
-    );
-  };
-  
-  const calculateAge = (birthDetails, currentYear, currentMonth, currentDate) => {
-    let years = currentYear - birthDetails.year;
-    let months, days;
-  
-    if (currentMonth < birthDetails.month) {
-      years--;
-      months = 12 - (birthDetails.month - currentMonth);
+  }
+};
+
+const adjacentCheck = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const possibleMovesCheck = () => {
+  for (let i in matrix) {
+    if (adjacentCheck(matrix[i])) {
+      return true;
+    }
+    let colarr = [];
+    for (let j = 0; j < columns; j++) {
+      colarr.push(matrix[i][j]);
+    }
+    if (adjacentCheck(colarr)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const randomPosition = (arr) => {
+  return Math.floor(Math.random() * arr.length);
+};
+
+const hasEmptyBox = () => {
+  for (let r in matrix) {
+    for (let c in matrix[r]) {
+      if (matrix[r][c] == 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const gameOverCheck = () => {
+  if (!possibleMovesCheck()) {
+    coverScreen.classList.remove("hide");
+    container.classList.add("hide");
+    overText.classList.remove("hide");
+    result.innerText = `Final score: ${score}`;
+    startButton.innerText = "Restart Game";
+  }
+};
+
+const generateTwo = () => {
+  if (hasEmptyBox()) {
+    let randomRow = randomPosition(matrix);
+    let randomCol = randomPosition(matrix[randomPosition(matrix)]);
+    if (matrix[randomRow][randomCol] == 0) {
+      matrix[randomRow][randomCol] = 2;
+      let element = document.querySelector(
+        `[data-position = '${randomRow}_${randomCol}']`
+      );
+      element.innerHTML = 2;
+      element.classList.add("box-2");
     } else {
-      months = currentMonth - birthDetails.month;
+      generateTwo();
     }
-  
-    if (currentDate < birthDetails.date) {
-      months--;
-      const lastMonth = currentMonth === 1 ? 12 : currentMonth - 1;
-      const daysInLastMonth = getDaysInMonth(lastMonth, currentYear);
-      days = daysInLastMonth - (birthDetails.date - currentDate);
+  } else {
+    gameOverCheck();
+  }
+};
+
+const generateFour = () => {
+  if (hasEmptyBox()) {
+    let randomRow = randomPosition(matrix);
+    let randomCol = randomPosition(matrix[randomPosition(matrix)]);
+    if (matrix[randomRow][randomCol] == 0) {
+      matrix[randomRow][randomCol] = 4;
+      let element = document.querySelector(
+        `[data-position= '${randomRow}_${randomCol}']`
+      );
+      element.innerHTML = 4;
+      element.classList.add("box-4");
     } else {
-      days = currentDate - birthDetails.date;
+      generateFour();
     }
-    return { years, months, days };
+  } else {
+    gameOverCheck();
+  }
+};
+
+const removeZero = (arr) => arr.filter((num) => num);
+const checker = (arr, reverseArr = false) => {
+  arr = reverseArr ? removeZero(arr).reverse() : removeZero(arr);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      arr[i] += arr[i + 1];
+      arr[i + 1] = 0;
+      score += arr[i];
+    }
+  }
+
+  arr = reverseArr ? removeZero(arr).reverse() : removeZero(arr);
+
+  let missingCount = 4 - arr.length;
+  while (missingCount > 0) {
+    if (reverseArr) {
+      arr.unshift(0);
+    } else {
+      arr.push(0);
+    }
+    missingCount -= 1;
+  }
+  return arr;
+};
+
+const slideDown = () => {
+  for (let i = 0; i < columns; i++) {
+    let num = [];
+    for (let j = 0; j < rows; j++) {
+      num.push(matrix[j][i]);
+    }
+    num = checker(num, true);
+    for (let j = 0; j < rows; j++) {
+      matrix[j][i] = num[j];
+      let element = document.querySelector(`[data-position='${j}_${i}']`);
+      element.innerHTML = matrix[j][i] ? matrix[j][i] : "";
+      element.classList.value = "";
+      element.classList.add("box", `box-${matrix[j][i]}`);
+    }
+  }
+
+  let decision = Math.random() > 0.5 ? 1 : 0;
+  if (decision) {
+    setTimeout(generateFour, 200);
+  } else {
+    setTimeout(generateTwo, 200);
+  }
+};
+
+const slideUp = () => {
+  for (let i = 0; i < columns; i++) {
+    let num = [];
+    for (let j = 0; j < rows; j++) {
+      num.push(matrix[j][i]);
+    }
+    num = checker(num);
+    for (let j = 0; j < rows; j++) {
+      matrix[j][i] = num[j];
+      let element = document.querySelector(`[data-position = '${j}_${i}']`);
+      element.innerHTML = matrix[j][i] ? matrix[j][i] : "";
+      element.classList.value = "";
+      element.classList.add("box", `box-${matrix[j][i]}`);
+    }
+  }
+  let decision = Math.random() > 0.5 ? 1 : 0;
+  if (decision) {
+    setTimeout(generateFour, 200);
+  } else {
+    setTimeout(generateTwo, 200);
+  }
+};
+
+const slideRight = () => {
+  for (let i = 0; i < rows; i++) {
+    let num = [];
+    for (let j = 0; j < columns; j++) {
+      num.push(matrix[i][j]);
+    }
+    num = checker(num, true);
+    for (let j = 0; j < columns; j++) {
+      matrix[i][j] = num[j];
+      let element = document.querySelector(`[data-position = '${i}_${j}']`);
+      element.innerHTML = matrix[i][j] ? matrix[i][j] : "";
+      element.classList.value = "";
+      element.classList.add("box", `box-${matrix[i][j]}`);
+    }
+  }
+  let decision = Math.random() > 0.5 ? 1 : 0;
+  if (decision) {
+    setTimeout(generateFour, 200);
+  } else {
+    setTimeout(generateTwo, 200);
+  }
+};
+
+const slideLeft = () => {
+  for (let i = 0; i < rows; i++) {
+    let num = [];
+    for (let j = 0; j < columns; j++) {
+      num.push(matrix[i][j]);
+    }
+
+    num = checker(num);
+    for (let j = 0; j < columns; j++) {
+      matrix[i][j] = num[j];
+      let element = document.querySelector(`[data-position = '${i}_${j}']`);
+      element.innerHTML = matrix[i][j] ? matrix[i][j] : "";
+      element.classList.value = "";
+      element.classList.add("box", `box-${matrix[i][j]}`);
+    }
+  }
+  let decision = Math.random() > 0.5 ? 1 : 0;
+  if (decision) {
+    setTimeout(generateFour, 200);
+  } else {
+    setTimeout(generateTwo, 200);
+  }
+};
+
+document.addEventListener("keyup", (e) => {
+  if (e.code == "ArrowLeft") {
+    slideLeft();
+  } else if (e.code == "ArrowRight") {
+    slideRight();
+  } else if (e.code == "ArrowUp") {
+    slideUp();
+  } else if (e.code == "ArrowDown") {
+    slideDown();
+  }
+  document.getElementById("score").innerText = score;
+});
+
+grid.addEventListener("touchstart", (event) => {
+  isSwiped = true;
+  getXY(event);
+  initialX = touchX;
+  initialY = touchY;
+});
+
+grid.addEventListener("touchmove", (event) => {
+  if (isSwiped) {
+    getXY(event);
+    let diffX = touchX - initialX;
+    let diffY = touchY - initialY;
+    if (Math.abs(diffY) > Math.abs(diffX)) {
+      swipeDirection = diffX > 0 ? "down" : "up";
+    } else {
+      swipeDirection = diffX > 0 ? "right" : "left";
+    }
+  }
+});
+
+grid.addEventListener("touchend", () => {
+  isSwiped = false;
+  let swipeCalls = {
+    up: slideUp,
+    down: slideDown,
+    left: slideLeft,
+    right: slideRight,
   };
-  
-  const getDaysInMonth = (month, year) => {
-    const isLeapYear = year % 4 === 0 && (year % 100 != 0 || year % 400 === 0);
-    const getDaysInMonth = [
-      31,
-      isLeapYear ? 29 : 28,
-      31,
-      30,
-      31,
-      30,
-      31,
-      31,
-      30,
-      31,
-      30,
-      31        
-    ];
-    return getDaysInMonth[month - 1];
-  };
-  
-  const displayResult = (bdate, bMonth, bYear) => {
-    document.getElementById("years").textContent = bYear;
-    document.getElementById("months").textContent = bMonth;
-    document.getElementById("days").textContent = bdate;
-  };
-  
-  document.getElementById("calc-age-btn").addEventListener("click", ageCalculate);
+  swipeCalls[swipeDirection]();
+  document.getElementById("score").innerText = score;
+});
+
+const startGame = () => {
+  score = 0;
+  document.getElementById("score").innerText = score;
+  grid.innerHTML = "";
+  matrix = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  container.classList.remove("hide");
+  coverScreen.classList.add("hide");
+  createGrid();
+  generateTwo();
+  generateTwo();
+};
+
+startButton.addEventListener("click", () => {
+  startGame();
+  swipeDirection = "";
+});
