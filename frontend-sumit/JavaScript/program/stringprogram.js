75d0acdcb41c22ba706a8e105c3f57df9c0845ec
PayalPatil8980 <sq.payal30@gmail.com>
2025-03-18 17:09:19 +0530

    18-03-2025

---
+++ frontend-sumit/JavaScript/program/stringprogram.js
@@ -0,0 +1,272 @@
+//1. JavaScript Program To Print Hello World
+
+{
+    // let str = `Hello World`
+    // console.log(str);
+
+}
+
+
+//2.JavaScript Program to Add Two Numbers
+
+{
+    // let n1 = 10
+    // let n2 = 20
+    // sum = n1 + n2
+    // console.log(sum);
+
+}
+
+
+//3.JavaScript Program to Find the Square Root
+
+{
+    // let n = 25
+    // g = n
+    // let object = 0
+    // while ((g - n / g) > object) {
+    //     g = (g + n / g) / 2
+    // }
+    // console.log(g);
+
+}
+
+{
+    // let a = 10
+
+    // sum =a*a
+    // console.log(`square of a is : ${sum}`);
+
+}
+
+// 4.JavaScript Program to Calculate the Area of a Triangle
+// santex:
+// 1/2 *b*h
+
+{
+    // let b = 10
+    // let h = 10
+
+    // let tri = 1/2 * b * h
+
+    // console.log(`area of triangle : ${tri}`);
+
+}
+
+//5.JavaScript Program to Swap Two Variables
+
+{
+    // let x = 10
+    // let y = 20
+
+    // x = x + y
+    // y = x - y
+    // x = x - y
+
+    // console.log(`now x value is: ${x}`);
+    // console.log(`now y value is: ${y}`);
+
+}
+
+// 6.JavaScript Program to Solve Quadratic Equation
+
+{
+
+
+
+
+
+
+
+}
+
+
+//7.JavaScript Program to Convert Kilometres to Miles
+
+{
+    // let km=10
+    // let miles=0.621371
+
+    // let mile= km * miles
+    // console.log(`${km} Kilometres = ${mile} Miles`);
+
+}
+
+//8.Javascript Program to Convert Celsius to Fahrenheit
+
+{
+    // let c = 10
+    // let f = (c * (9 / 5)) + 32
+
+    // console.log(`${c} Celsius = ${f} Fahrenheit`);
+
+}
+
+//9.Javascript Program to Generate a Random Number
+
+// using Math.random() function
+{
+
+    // console.log(Math.random()*100);
+
+}
+
+//10.Javascript Program to Check if a number is Positive, Negative, or Zero
+
+{
+    // let num = 0
+
+    // if (num > 0) {
+    //     console.log(`Number is Positive`);
+    // } else if (num < 0) {
+    //     console.log(`Number is Negative`);
+    // } else {
+    //     console.log(`Number is Zero`);
+    // }
+}
+
+//11.Javascript Program to Check if a Number is Odd or Even
+
+{
+    // let num=3
+
+    // if(num %= 2){
+    //     console.log(`Number is Odd`)
+    // }else{
+    //     console.log(`Number is Even`);
+
+    // }
+}
+
+//12.JavaScript Program to Find the Largest Among Three Numbers
+
+{
+    // let n1 = 10
+    // let n2 = 30
+    // let n3 = 200
+
+    // if (n1 > n2 && n1 > n3) {
+    //     console.log(`${n1}(n1) is Largest Numbers`);
+    // } else if (n2 > n1 && n2 > n3) {
+    //     console.log(`${n2}(n2) is Largest Numbers`);
+    // } else {
+    //     console.log(`${n3}(n3) is Largest Numbers`);
+    // }
+}
+
+//13.JavaScript Program to Check Prime Number
+
+{
+
+
+
+
+
+}
+
+//14.JavaScript Program to Print All Prime Numbers in an Interval
+
+{
+
+
+
+
+
+}
+
+//15.JavaScript Program to Find the Factorial of a Number
+
+{
+    // let num =10
+
+    // let fac=1
+
+    // for(let i = 0;i<num ; i++){
+    //     fac = fac * (num-i)
+    // }
+    // console.log(`factorial of ${num} is = ${fac}`);
+
+}
+
+//16.JavaScript Program to Display the Multiplication Table
+
+{
+    // let num = 69
+
+    // for (let i = 1; i <= 10; i++) {
+    //     let Table = num * i
+    //     console.log(`${num} * ${i} = ${Table}`);
+    // }
+}
+
+//17.JavaScript Program to Print the Fibonacci Sequence
+
+{
+    // let n = 20
+    // let f1=0
+    // let f2=1
+    // console.log(`first ${n} numbers fibbonacci is: ${f1}`);
+    // console.log(`first ${n} numbers fibbonacci is: ${f2}`);
+    // for(let i=1 ;i<=n ; i++ ){
+
+    //     fibbo = f1 + f2
+    //     f1 = f2
+    //     f2 = fibbo
+    //     console.log(`first ${n} numbers fibbonaci is : ${fibbo}`);
+
+    // }
+}
+
+//18.JavaScript Program to Check Armstrong Number
+
+{
+
+
+
+
+
+}
+
+//19.JavaScript Program to Find Armstrong Number in an Interval
+
+{
+
+
+
+
+
+}
+
+//20.JavaScript Program to Make a Simple Calculator
+
+{
+    // let num1 = parseInt(prompt(`Enter number 1`))
+    // let num2 = parseInt(prompt(`Enter number 2`))
+    // let calc = prompt(`Enter symbol for plus (+) ,
+    //                   for minus (-) , 
+    //                   for multiplication (*) , 
+    //                   for division (/) `)
+    // switch (calc) {
+    //     case '+':
+    //         let sum = num1 + num2
+    //         console.log(`${num1} + ${num2} = ${sum}`);
+    //         break;
+    //     case '-':
+    //         let sub = num1 - num2
+    //         console.log(`${num1} - ${num2} = ${sub}`);
+    //         break;
+    //     case '*':
+    //         let multi = num1 * num2
+    //         console.log(`${num1} * ${num2} = ${multi}`);
+    //         break;
+    //     case '/':
+    //         let div = num1 / num2
+    //         console.log(`${num1} / ${num2} = ${div}`);
+    //         break;
+    //     default:
+    //         console.log(`error`);
+    //         break;
+    // }
+}
+
+//21.
